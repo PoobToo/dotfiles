934ec3b37a475db772a52d566224b5d8939d7b83
Leo <leo@radish>
2026-05-02 15:07:16 -0700

    hardware and base config for onion

--- hosts/onion/configuration.nix
+++ hosts/onion/configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   imports = [
@@ -8,9 +8,22 @@
 
   networking.hostName = "onion";
 
-  # TODO: copy hardware-configuration.nix from onion here
-  # TODO: add LUKS device if applicable
-  # TODO: add any desktop-specific packages (GPU drivers, etc.)
+  # Swap LUKS device (root LUKS lives in hardware-configuration.nix)
+  boot.initrd.luks.devices."luks-fb485ae4-a2c1-4bf8-b98d-dac30301f41a".device =
+    "/dev/disk/by-uuid/fb485ae4-a2c1-4bf8-b98d-dac30301f41a";
+
+  # Using stock kernel for now TODO switch to cachyos kernel once initial setup is up
+  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_latest;
+
+  # Nvidia funtimes
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.graphics.enable = true;
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
   system.stateVersion = "25.11";
 }
