3f02755a03b6f61b59f5abfcdc5486c547442260
sotoleo <84047139+sotoleo@users.noreply.github.com>
2026-05-03 17:40:07 -0700

    extra monitor config onion-only

--- hosts/onion/home.nix
+++ hosts/onion/home.nix
@@ -1,4 +1,17 @@
 { lib, ... }:
 {
-  # Host-specific home-manager tweaks for onion go here.
+  programs.niri.settings.outputs = {
+    "LG Electronics LG ULTRAGEAR 109NTBKNT837" = {
+      mode = { width = 2560; height = 1440; refresh = 164.958; };
+      position = { x = 0; y = 0; };
+      scale = 1.0;
+      transform.rotation = 0;
+    };
+    "Acer Technologies XV271U M3 14040116D3LIJ" = {
+      mode = { width = 2560; height = 1440; refresh = 180.002; };
+      position = { x = 2560; y = -700; };
+      scale = 1.0;
+      transform.rotation = 270;
+    };
+  };
 }
