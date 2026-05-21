b8ff05ac0dd3febb42ce90e23dccd9a06a2155f6
sotoleo <84047139+sotoleo@users.noreply.github.com>
2026-05-20 18:22:49 -0700

    sunshine on radish

--- hosts/radish/configuration.nix
+++ hosts/radish/configuration.nix
@@ -11,5 +11,33 @@
   # LUKS
   boot.initrd.luks.devices."luks-b1ec9b3e-09fd-480c-a929-5171b0241b06".device = "/dev/disk/by-uuid/b1ec9b3e-09fd-480c-a929-5171b0241b06";
 
+  services.sunshine = {
+    enable = true;
+    openFirewall = true;
+    capSysAdmin = true;
+    autoStart = false;
+  };
+
+  systemd.services.lid-inhibit = {
+    description = "Inhibit lid-switch suspend (manual toggle)";
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = "${pkgs.systemd}/bin/systemd-inhibit "
+                + "--what=handle-lid-switch --who=lid-inhibit "
+                + "--why='manual override' --mode=block "
+                + "sleep infinity";
+    };
+  };
+
+  security.polkit.extraConfig = ''
+    polkit.addRule(function(action, subject) {
+      if (action.id == "org.freedesktop.systemd1.manage-units" &&
+          action.lookup("unit") == "lid-inhibit.service" &&
+          subject.user == "leo") {
+        return polkit.Result.YES;
+      }
+    });
+  '';
+
   system.stateVersion = "25.11";
 }
--- hosts/radish/home.nix
+++ hosts/radish/home.nix
@@ -1,4 +1,10 @@
 { lib, ... }:
 {
   programs.foot.settings.main.font = lib.mkForce "JetBrainsMono Nerd Font:size=10";
+
+  programs.fish.shellAliases = {
+    lid-inhibit = "systemctl start lid-inhibit";
+    lid-release = "systemctl stop lid-inhibit";
+    lid-status  = "systemctl is-active lid-inhibit";
+  };
 }
